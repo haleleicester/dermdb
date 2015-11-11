1dfedd0d6e35e28e3eb4c81dfc1cb4e45d6b088c
Samuel Mills <spam.mills@hotmail.com>
2015-11-11 11:27:36 +0000

    v0.3.1 - Fixed bug with cookie handling

--- middleware/auth/format.js
+++ middleware/auth/format.js
@@ -5,24 +5,29 @@ var Error = require('../../lib/errors/index.js');
 
 module.exports = function(req, res, next){
     jwt.verify(req.cookies.jwt, config.jwt.salt, function(err, decoded) {
-        mysql.query("SELECT * FROM users WHERE id = " + decoded.user.id, function(err, result){
-            if (err) {
-                var e = new Error.MySQLError(err);
-                next(e);
-            } else if (typeof result === "undefined" || result === [] || result.length === 0){
-                mysql.query("INSERT INTO users (id) VALUES (" + decoded.user.id + ")", function(err, result){
-                    if (err){
-                        var e = new Error.MySQLError(err);
-                        next(e);
-                    } else {
-                        res.locals.user = {id: decoded.user.id};
-                        next();
-                    }
-                })
-            } else {
-                res.locals.user = result[0];
-                next();
-            }
-        });
+        if (err){
+            res.locals.user = null;
+            next();
+        } else {
+            mysql.query("SELECT * FROM users WHERE id = " + decoded.user.id, function (err, result) {
+                if (err) {
+                    var e = new Error.MySQLError(err);
+                    next(e);
+                } else if (typeof result === "undefined" || result === [] || result.length === 0) {
+                    mysql.query("INSERT INTO users (id) VALUES (" + decoded.user.id + ")", function (err, result) {
+                        if (err) {
+                            var e = new Error.MySQLError(err);
+                            next(e);
+                        } else {
+                            res.locals.user = {id: decoded.user.id};
+                            next();
+                        }
+                    })
+                } else {
+                    res.locals.user = result[0];
+                    next();
+                }
+            });
+        }
     });
 };
